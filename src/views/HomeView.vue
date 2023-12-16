c6f59e0496b1adfdf84e8ca7236a4f26eee59d96
Batuhan Birkan <batuhan.birkan@ekip.co>
2023-12-16 10:39:13 +0300

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { curve, curves, ec } from "elliptic";
+import TheWelcome from "../components/TheWelcome.vue";
+const a = new ec("secp256k1");
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
